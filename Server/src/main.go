46d0e3476b747a7f36dc19f8dd73bdce8dc6c40c
HanwGeek <HanwGeek@gmail.com>
2020-10-04 17:57:58 +0800

    (Server): Modify files

--- Server/src/main.go
+++ Server/src/main.go
@@ -1,40 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"ogcservice"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/jonas-p/go-shp"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/wfs", ogcservice.WFSHandler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	r := gin.Default()
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	shape, err := shp.Open("./data/crop.shp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer shape.Close()
+	// fields from the attribute table (DBF)
+	fields := shape.Fields()
 
-	// shape, err := shp.Open("./data/crop.shp")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer shape.Close()
-	// // fields from the attribute table (DBF)
-	// fields := shape.Fields()
+	// loop through all features in the shapefile
+	for shape.Next() {
+		n, p := shape.Shape()
 
-	// // loop through all features in the shapefile
-	// for shape.Next() {
-	// 	n, p := shape.Shape()
+		// print feature
+		fmt.Fprintf(w, "%v %v", reflect.TypeOf(p).Elem(), p.BBox())
 
-	// 	// print feature
-	// 	fmt.Fprintf(w, "%v %v", reflect.TypeOf(p).Elem(), p.BBox())
-
-	// 	// print attributes
-	// 	for k, f := range fields {
-	// 		val := shape.ReadAttribute(n, k)
-	// 		fmt.Fprintf(w, "\t%v: %v\n", f, val)
-	// 	}
-	// 	fmt.Fprintf(w, "\n")
-	// }
+		// print attributes
+		for k, f := range fields {
+			val := shape.ReadAttribute(n, k)
+			fmt.Fprintf(w, "\t%v: %v\n", f, val)
+		}
+		fmt.Fprintf(w, "\n")
+	}
 }
