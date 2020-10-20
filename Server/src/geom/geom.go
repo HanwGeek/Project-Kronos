2cf829148304d6778bf4ff5ca4dba99a3a2fa204
Nithouson <sinesloop@pku.edu.cn>
2020-10-20 14:12:51 +0800

    增加Layer结构体和Geometry接口
    使用指针传递修正涉及修改属性的方法；添加示例（测试）代码；调整Point和Polygon方法

--- Server/src/geom/geom.go
+++ Server/src/geom/geom.go
@@ -18,3 +18,35 @@ const (
 	// KrGeometryCollection is a collection of heterogeneus geometries
 	KrGeometryCollection = 8
 )
+
+type Geometry interface{
+	GeomType() int
+}
+
+/*
+func main(){
+	l:=Layer{geomtype: KrPoint}
+	fmt.Println(l.IsEmpty())
+	p:=Point{Coord{3,5}}
+	fmt.Println(p.GeomType())
+	fmt.Println(l.GetGeomType())
+	l.AddFeature(p)
+	fmt.Println(l.feat)
+	fmt.Println(l.FeatureCount())
+
+	l2:=Layer{geomtype: KrPolygon}
+	po:=Polygon{}
+	outr:=LineString{[]Coord{Coord{1,2},Coord{4,7},Coord{-3,-3}}}
+	innr:=LineString{[]Coord{Coord{0.5,0.5},Coord{0.3,0.3},Coord{0.3,0.5}}}
+	po.AddRing(outr)
+	po.AddRing(innr)
+	l2.AddFeature(po)
+	l2.AddFeature(p)
+	fmt.Println(l2.FeatureCount())
+	fmt.Println(l2.GetFeature(0))
+	npo:=l2.GetFeature(0).(Polygon)
+	fmt.Println(npo.NInnerRings())
+	npo.DeleteRing(1)
+	fmt.Println(npo)
+}
+*/
--- Server/src/geom/layer.go
+++ Server/src/geom/layer.go
@@ -2,4 +2,43 @@ package geom
 
 // Layer means a collection of geoms
 type Layer struct {
+	geomtype int
+	feat []Geometry
+}
+
+func (l Layer) GetGeomType () int {
+	return l.geomtype
+}
+
+func (l Layer) IsEmpty () bool {
+	return len(l.feat)==0
+}
+
+func (l Layer) FeatureCount () int {
+	return len(l.feat)
+}
+
+func (l Layer) GetFeature (n int) Geometry {
+	m := len(l.feat)
+	if n > m-1 {
+		return l.feat[m-1]
+	}
+	return l.feat[n]
+}
+
+// Add a feature at the end
+func (l *Layer) AddFeature(g Geometry) {
+	if l.geomtype!=g.GeomType() {
+		return
+	}
+	l.feat=append(l.feat,g)
+}
+
+// Delete a point at index n
+func (l *Layer) DeleteFeature(n int) {
+	m := len(l.feat)
+	if n > m - 1 {
+		return
+	}
+	l.feat = append(l.feat[:n], l.feat[n+1:]...)
 }
