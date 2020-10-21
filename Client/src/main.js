4c4690c49746e2bcf9e7fb412697564d7b8874a4
HanwGeek <HanwGeek@gmail.com>
2020-10-21 22:53:18 +0800

    (Client): Add tools

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import x2js from 'x2js'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+Vue.prototype.$http = axios;
+Vue.prototype.$bus = new Vue();
+Vue.prototype.$x2js = new x2js();
 
 new Vue({
   render: h => h(App),
