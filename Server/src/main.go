21f772b351b9dfa6c8176aea67c3112a4029c7c3
HanwGeek <HanwGeek@gmail.com>
2020-11-08 18:13:23 +0800

    (Server): Add CORS

--- Server/src/main.go
+++ Server/src/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/gin-contrib/cors"
+	geojson "github.com/paulmach/go.geojson"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +14,22 @@ func main() {
 	// ogcservice.Connect()
 	r := gin.Default()
 
+	// Allow CORS
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
+
 	file, err := ioutil.ReadFile("./data/crop.json")
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, file)
+
+	data, err := geojson.UnmarshalFeatureCollection(file)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	r.GET("/data", func(c *gin.Context) {
+		c.JSON(200, data)
 
 	})
 
